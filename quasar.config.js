3dd0c6b0d34ff8453b79c4a12789bb05db22d5bf
Akayy <techakayy@gmail.com>
2024-02-15 21:33:55 +1100

    Added icon-mapping to use unocss icons

--- quasar.config.js
+++ quasar.config.js
@@ -23,7 +23,7 @@ module.exports = configure(function (ctx) {
     },
 
     // https://v2.quasar.dev/quasar-cli-vite/prefetch-feature
-    // preFetch: true,
+    preFetch: true,
 
     // app boot file (/src/boot)
     // --> boot files are part of "main.js"
@@ -40,7 +40,7 @@ module.exports = configure(function (ctx) {
     // https://github.com/quasarframework/quasar/tree/dev/extras
     extras: [
       // 'ionicons-v4',
-      // 'mdi-v5',
+      'mdi-v5',
       // 'fontawesome-v6',
       // 'eva-icons',
       // 'themify',
@@ -53,6 +53,8 @@ module.exports = configure(function (ctx) {
 
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js#framework
     framework: {
+      iconSet: 'mdi-v5',
+      // iconSet: 'svg-mdi-v5',
       config: {},
       // Make spacing utilities responsive
       cssAddon: true,
--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
   import 'uno.css'
+  import { useQuasar } from 'quasar'
+  const $q = useQuasar()
+  $q.iconMapFn = (iconName) => {
+    return {
+      // If using other iconsets, use BaseIcon.vue component instead of q-icon.
+      // To use BaseIcon, refer to next step for further details.
+      // i-mdi-home (unocss icons) will map to mdi-home (quasar mdi icons)
+      icon: iconName.startsWith('i-') ? iconName.substring(2) : iconName,
+    }
+  }
 
   // Set brand
   const { setTheme } = useTheme()
@@ -7,5 +17,8 @@
 </script>
 
 <template>
-  <router-view />
+  <div>
+    <HeadAndMeta />
+    <router-view />
+  </div>
 </template>
